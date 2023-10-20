9f66553141b91d78614229a705cb432d8866756e
hrishikesh147 <hrishikeshbhagawati@gmail.com>
2023-10-20 19:34:51 +0530

    data ingestion pipeline completed

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.ML_Part_predict import logger
+
+logger.info("welcome to custom logger")
---
+++ src/ML_Part_predict/utils/common.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import yaml
+from box import ConfigBox
+from src.ML_Part_predict import logger
+from ensure import ensure_annotations
+import os,sys
+import json
+
+@ensure_annotations
+def read_yaml(filepath:Path) -> ConfigBox:
+    try:
+        with open(filepath) as y:
+            content=yaml.safe_load(y)
+            logger.info(f"yaml file {content} loaded successfully")
+            return ConfigBox(y)
+        
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directories(filep:list, verbose=True):
+    for i in filep:
+        os.makedirs(i,exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at path {filep}")
+
+@ensure_annotations
+def save_json(save_in_path:Path,data:dict):
+    with open(save_in_path,"w") as f:
+        json.dump(data,f,indent=4)
+    logger.info(f"Json file saved at {save_in_path}")
+
+
+@ensure_annotations
+def load_json(load_from_path:Path):
+    with open(load_from_path,"r") as f:
+        content=json.load(f)
+    logger.info(f"json file {load_from_path} loaded")
+    return ConfigBox(content)
+
+@ensure_annotations
+def get_size(path:Path) -> str:
+    size_kb=round(os.path.getsize(path)/1024)
+    logger.info(f"size of {path} is {size_kb}")
+    return f"{size_kb} KB"
